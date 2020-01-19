1c29eaff0911f452b7c64981873c7896e5e556cf
swastikdas4 <60071906+swastikdas4@users.noreply.github.com>
2020-01-19 23:06:02 +0530

    Add files via upload

---
+++ Assets/script/Addplayercontrolledvelocity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Addplayercontrolledvelocity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Vector3 v3Force;
+    [SerializeField]
+    KeyCode keypositive;
+    [SerializeField]
+    KeyCode keynegative;
+
+
+    void Update()
+    {
+        if (Input.GetKey(keypositive))
+            GetComponent<Rigidbody>().velocity += v3Force;
+
+        if (Input.GetKey(keynegative))
+            GetComponent<Rigidbody>().velocity -= v3Force;
+    }
+}
---
+++ Assets/script/addvelocity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addvelocity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Vector3 v3Force;
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity += v3Force;
+    }
+}
